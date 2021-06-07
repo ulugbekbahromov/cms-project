d4f65bdaa0de12396e828d29bf5047b407e95ba3
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-07 16:09:34 +0500

    created basic ops

---
+++ admin/categories.php
@@ -0,0 +1,89 @@
+
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+
+                       <div class="col-xs-6">
+
+                           <?php
+
+                           //Insert query
+                           insert_categories();
+                           ?>
+
+                           <form action="" method="post">
+                               <div class="form-group">
+                                   <label for="cat_title">Add Category</label>
+                                   <input type="text" class="form-control" name="cat_title">
+                               </div>
+                               <div class="form-group">
+                                   <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                               </div>
+
+
+                           </form>
+
+                           <?php
+
+                           if(isset($_GET['edit'])){
+
+                               $cat_id = $_GET['edit'];
+                               include "includes/admin_update.php";
+
+                           }
+
+                           ?>
+
+
+                       </div>
+
+                        <div class="col-xs-6">
+
+
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                <?php
+                                // Find all categories
+                                find_categories();
+
+                                ?>
+
+                                <?php
+                                //Delete query
+                                delete_categories();
+                                ?>
+
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>
